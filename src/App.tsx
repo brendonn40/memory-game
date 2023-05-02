c79b065d1012ea2075af1aeb32dddb2ea6e5c083
Brendon <brendoncesar40@gmail.com>
2023-05-02 20:47:29 -0300

    new images

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import { ThemeProvider } from "./ThemeProvider";
 import Card from "./components/card";
-
+import { Flex, Grid, SimpleGrid } from '@mantine/core';
 const characters = [
-  {image:'/homer.png',name:'Homer Simpson'},
-  {image:'/lisa.jpg',name:'Lisa Simpson'},
+  {image:'/HomerSimpson.webp',name:'Homer Simpson'},
+  {image:'/lisa.png',name:'Lisa Simpson'},
   {image:'/bart.png',name:'Bart Simpson'},
-  {image:'/marge.png',name:'Marge Simpson'},
-  {image:'/abraham.png',name:'Abraham Simpson'},
+  {image:'/abraham.webp',name:'Abraham Simpson'},
+  {image:'/sideshow_bob.webp',name:'Sideshow Bob'},
+  {image:'/Edna Krabappel.png',name:'Edna Krabappel'},
+  {image:'/Waylon Smithers.png',name:'Waylon Smithers'},
+  {image:'/Krustytheclown.png',name:'Krusty'},
+  {image:'/sanders.png',name:'Ned Flanders'},
+  {image:'/nelson.png',name:'Nelson Muntz'},
+  {image:'/Milhouse_Van_Houten_TS.webp',name:'Milhouse Van Houten'},
+  {image:'/Marge_Simpson.webp',name:'Marge Simpson'},
 ]
 export default function App() {
   return (
     <ThemeProvider>
+      <SimpleGrid cols={4}>
       {characters.map((item)=>(
-        <Card image={item.image} name={item.name}/>
+        <Flex justify={`center`} align={`center`}>
+
+          <Card image={item.image} name={item.name}/>
+        </Flex>
       ))}
-      {/* <Card image="/homer.png" name="Homer Simpson"/> */}
+      </SimpleGrid>
+      
     </ThemeProvider>
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,8 @@
-import { Box,Image,Text } from "@mantine/core";
+import { Image,Text } from "@mantine/core";
 
 function Card({image,name}:{image:string,name:string}) {
     return ( <div style={{display:`inline-block`}}>
-        <Image maw={200} src={image}/>
+        <Image maw={150} src={image}/>
         <Text align="center">{name}</Text>
     </div> );
 }
